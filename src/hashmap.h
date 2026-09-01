281e1c6b505da44ca8c06693ab9ea31ddf4d8753
Adrian Hornung <adrian.hornung@yahoo.de>
2026-09-01 09:48:20 -0400

    Implement Robin Hood Style Hashmap

---
+++ src/hashmap.h
@@ -0,0 +1,119 @@
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "hashmap_hasher.h"
+
+/* HASHMAP */
+#define HASHMAP_DEFAULT_CAPACITY 16
+#define HASHMAP_LOAD_FACTOR_PERCENT 80
+
+struct HashMapSlotHeader {
+  Hash hash;
+  bool occupied;
+} __attribute__((packed));
+
+typedef struct HashMapSlotHeader HashMapSlotHeader;
+
+struct HashMapSlot {
+  HashMapSlotHeader *header;
+  void *key;
+  void *value;
+};
+
+typedef struct HashMapSlot HashMapSlot;
+
+struct HashMapInternal {
+  size_t len;
+  size_t capacity;
+  HashMapAlgorithm hash_algo;
+  HashMapHashFn hash_fn;
+  HashMapEqFn eq_fn;
+  size_t key_size;
+  size_t value_size;
+  // Data := HashMapSlotHeader KEY VALUE * capacity
+  // ┌─────────────── entry 0 ───────────────────┐
+  // │ hash │ occupied │ key bytes │ value bytes │
+  // ├─────────────── entry 1 ───────────────────┤
+  // │ hash │ occupied │ key bytes │ value bytes │
+  // ├───────────────────────────────────────────┤
+  // │ ...                                       │
+  // └───────────────────────────────────────────┘
+  uint8_t *data;
+};
+
+typedef struct HashMapInternal HashMapInternal;
+
+#define HashMap(key_type, value_type)                                          \
+  union {                                                                      \
+    HashMapInternal internal;                                                  \
+    /* NOLINTNEXTLINE bugprone-macro-parentheses */                            \
+    key_type *key_payload;                                                     \
+    /* NOLINTNEXTLINE bugprone-macro-parentheses */                            \
+    value_type *value_payload;                                                 \
+  }
+
+#define hashmap_key_size(map) sizeof(*(map)->key_payload)
+
+#define hashmap_value_size(map) sizeof(*(map)->value_payload)
+
+#define hashmap_key_ptr(map, key)                                              \
+  (const void *)(1 ? &(key) : (map)->key_payload)
+
+#define hashmap_value_ptr(map, value)                                          \
+  (const void *)(1 ? &(value) : (map)->value_payload)
+
+void hashmap_init_with_algo_impl(HashMapInternal *map, HashMapAlgorithm algo,
+                                 HashMapHashFn hash_fn, HashMapEqFn eq_fn,
+                                 size_t key_size, size_t value_size);
+
+#define hashmap_init_with_algo(map, algo, hash_fn, eq_fn)                      \
+  hashmap_init_with_algo_impl((&(map)->internal), (algo), (hash_fn), (eq_fn),  \
+                              hashmap_key_size(map), hashmap_value_size(map))
+
+void hashmap_init_impl(HashMapInternal *map, HashMapHashFn hash_fn,
+                       HashMapEqFn eq_fn, size_t key_size, size_t value_size);
+
+#define hashmap_init(map, hash_fn, eq_fn)                                      \
+  hashmap_init_impl((&(map)->internal), (hash_fn), (eq_fn),                    \
+                    hashmap_key_size(map), hashmap_value_size(map))
+
+void hashmap_free_impl(HashMapInternal *map);
+
+#define hashmap_free(map) hashmap_free_impl((&(map)->internal))
+
+// Will try to insert the key into the map will return true if there already
+// was a key previously inserted into the db
+bool hashmap_put_impl(HashMapInternal *map, const void *key, const void *value);
+
+#define hashmap_put(map, key, value)                                           \
+  hashmap_put_impl((&(map)->internal), hashmap_key_ptr((map), key),            \
+                   hashmap_value_ptr((map), value))
+
+void *hashmap_get_impl(HashMapInternal *map, const void *key);
+
+#define hashmap_get(map, key)                                                  \
+  ((typeof((map)->value_payload))hashmap_get_impl(                             \
+      &(map)->internal, hashmap_key_ptr((map), key)))
+
+bool hashmap_remove_impl(HashMapInternal *map, const void *key);
+
+#define hashmap_remove(map, key)                                               \
+  hashmap_remove_impl(&(map)->internal, hashmap_key_ptr((map), key))
+
+size_t hashmap_len_impl(const HashMapInternal *map);
+
+#define hashmap_len(map) hashmap_len_impl(&(map)->internal)
+
+/* HASH FUNCTIONS */
+
+// cstr
+bool hashmap_equal_cstr(const void *a, const void *b, size_t key_size);
+
+// for int, uint64_t enum Foo etc...
+bool hashmap_equal_bytes(const void *a, const void *b, size_t key_size);
+
+#endif
---
+++ src/hashmap_hasher.h
@@ -0,0 +1,76 @@
+#ifndef HASHMAP_HASHER_H
+#define HASHMAP_HASHER_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/* HASHING FUNCTIONS */
+#define HASHMAP_HASH_CONTEXT_MAX_SIZE 64
+
+typedef uint64_t Hash;
+
+// VTable Alias for hash builder
+typedef void (*HashMapContextInit)(void *ctx);
+typedef void (*HashMapContextUpdate)(void *ctx, const void *data, size_t size);
+typedef Hash (*HashMapContextFinalize)(void *ctx);
+
+struct HashMapHashBuilder {
+  HashMapContextUpdate update;
+  uint8_t ctx[HASHMAP_HASH_CONTEXT_MAX_SIZE];
+};
+
+typedef struct HashMapHashBuilder HashMapHashBuilder;
+
+struct HashMapAlgorithm {
+  // VTable for hash builder
+  HashMapContextInit init;
+  HashMapContextUpdate update;
+  HashMapContextFinalize finalize;
+};
+
+typedef struct HashMapAlgorithm HashMapAlgorithm;
+
+/* HASHING FNV1A FUNCTIONS */
+struct Fnv1aContext {
+  uint64_t state;
+};
+
+typedef struct Fnv1aContext Fnv1aContext;
+
+void hashmap_fnv1a_init(void *ctx);
+void hashmap_fnv1a_update(void *ctx, const void *data, size_t size);
+Hash hashmap_fnv1a_finalize(void *ctx);
+
+extern const HashMapAlgorithm Fnv1a;
+
+/* HASHING HELPER FUNCTIONS */
+#define HASHMAP_INTEGER_TYPES(X)                                               \
+  X(uint8_t, u8)                                                               \
+  X(uint16_t, u16)                                                             \
+  X(uint32_t, u32)                                                             \
+  X(uint64_t, u64)                                                             \
+  X(int8_t, i8)                                                                \
+  X(int16_t, i16)                                                              \
+  X(int32_t, i32)                                                              \
+  X(int64_t, i64)                                                              \
+  X(int, int)
+
+typedef void (*HashMapHashFn)(HashMapHashBuilder *builder, const void *key,
+                              size_t key_size);
+
+typedef bool (*HashMapEqFn)(const void *a, const void *b, size_t key_size);
+
+#define HASHMAP_DEFINE_HASH_UPDATE(type, suffix)                               \
+  void hashmap_hash_##suffix(HashMapHashBuilder *builder, const void *key,     \
+                             size_t key_size);
+
+HASHMAP_INTEGER_TYPES(HASHMAP_DEFINE_HASH_UPDATE)
+#undef HASHMAP_DEFINE_HASH_UPDATE
+
+void hashmap_hash_blob(HashMapHashBuilder *builder, const void *key,
+                       size_t key_size);
+
+void hashmap_hash_string(HashMapHashBuilder *builder, const void *key,
+                         size_t key_size);
+#endif
